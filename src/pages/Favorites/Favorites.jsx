f48c4fb92063c04dc62f24ec7135130f732f832a
marycala <marycala87@gmail.com>
2025-03-09 18:19:14 +0100

    add useReducer

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,23 +1,37 @@
-import React, { useState, useEffect } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import CharacterCard from '../../components/CharacterCard/CharacterCard'
 import './Favorites.css'
 import { useNavigate } from 'react-router-dom'
 
+const favoritesReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_FAVORITE':
+      return [...state, action.payload]
+    case 'REMOVE_FAVORITE':
+      return state.filter((char) => char._id !== action.payload)
+    default:
+      return state
+  }
+}
+
 const Favorites = () => {
   const navigate = useNavigate()
-  const [favorites, setFavorites] = useState([])
+
+  const [favorites, dispatch] = useReducer(
+    favoritesReducer,
+    JSON.parse(localStorage.getItem('favorites')) || []
+  )
 
   useEffect(() => {
-    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || []
-    setFavorites(storedFavorites)
-  }, [])
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+  }, [favorites])
 
-  const removeFavorite = (characterId) => {
-    const updatedFavorites = favorites.filter(
-      (char) => char._id !== characterId
-    )
-    setFavorites(updatedFavorites)
-    localStorage.setItem('favorites', JSON.stringify(updatedFavorites))
+  const toggleFavorite = (character) => {
+    if (favorites.some((fav) => fav._id === character._id)) {
+      dispatch({ type: 'REMOVE_FAVORITE', payload: character._id })
+    } else {
+      dispatch({ type: 'ADD_FAVORITE', payload: character })
+    }
   }
 
   return (
@@ -30,12 +44,12 @@ const Favorites = () => {
         <p>No favorites yet</p>
       ) : (
         <div className='favorites-grid'>
-          {favorites.map((character) => (
+          {favorites.map((character, index) => (
             <CharacterCard
-              key={character._id}
+              key={character._id ? String(character._id) : `char-${index}`}
               character={character}
               isFavorite={true}
-              toggleFavorite={() => removeFavorite(character._id)}
+              toggleFavorite={() => toggleFavorite(character)}
             />
           ))}
         </div>
