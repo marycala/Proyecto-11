ee65cc7fd80d504f08ae02cf0deda44972446327
marycala <marycala87@gmail.com>
2025-03-09 17:27:02 +0100

    change imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Characters from './pages/Characters/Characters'
 import Character from './pages/Character/Character'
-import SearchResults from './pages/SearchResults/SearchResults'
-import Favorites from './components/Favorites/Favorites'
+import SearchResults from './components/SearchResults/SearchResults'
+import Favorites from './pages/Favorites/Favorites'
 
 const App = () => {
   return (
